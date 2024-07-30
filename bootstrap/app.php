1fd524ccae7efc11247a92d6c76d8996a9055489
Fady Mondy <info@3x1.io>
2024-07-30 20:21:31 +0300

    Add error logger

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,5 +21,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        \TomatoPHP\FilamentDiscord\Helpers\DiscordErrorReporter::make($exceptions);
     })->create();
