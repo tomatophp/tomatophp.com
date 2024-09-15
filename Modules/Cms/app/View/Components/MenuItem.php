223deac1a8fd75b6a613627abe0731097204093c
Fady Mondy <info@3x1.io>
2024-09-15 14:41:40 +0300

    fix register

--- Modules/Cms/app/View/Components/MenuItem.php
+++ Modules/Cms/app/View/Components/MenuItem.php
@@ -14,6 +14,7 @@ class MenuItem extends Component
     public function __construct(
         public string $label,
         public ?string $url = null,
+        public ?string $target = null,
     )
     {
         //
--- Modules/Cms/resources/views/parts/menu.blade.php
+++ Modules/Cms/resources/views/parts/menu.blade.php
@@ -2,7 +2,7 @@
 
 @if($menu)
     @foreach($menu as $item)
-        <x-cms-menu-item label="{{ $item->title[app()->getLocale()] }}" url="{{ $item->url }}" />
+        <x-cms-menu-item label="{{ $item->title[app()->getLocale()] }}" url="{{ $item->url }}" target="{{ $item->target }}" />
     @endforeach
 @endif
 
