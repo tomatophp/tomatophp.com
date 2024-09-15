7d28ba7be954dba8c8861daf81457e2757a83b25
Fady Mondy <info@3x1.io>
2024-09-15 14:43:52 +0300

    fix register

--- Modules/Cms/resources/views/parts/menu.blade.php
+++ Modules/Cms/resources/views/parts/menu.blade.php
@@ -2,7 +2,7 @@
 
 @if($menu)
     @foreach($menu as $item)
-        <x-cms-menu-item label="{{ $item->title[app()->getLocale()] }}" url="{{ $item->url }}" target="{{ $item->target }}" />
+        <x-cms-menu-item label="{{ $item->title[app()->getLocale()] }}" url="{{ $item->url }}" target="{{ $item->new_tab ? '_blank' : null }}" />
     @endforeach
 @endif
 
