62e495329ab052a8f1d570540bf58c2c03a7b5c0
Fady Mondy <info@3x1.io>
2024-10-01 15:51:53 +0300

    add comments and reacts options

--- Modules/Cms/resources/views/post.blade.php
+++ Modules/Cms/resources/views/post.blade.php
@@ -27,11 +27,16 @@
             <section class="container mx-auto py-6 rounded-lg px-6 sm:px-6 max-w-3xl prose prose-lg lg:prose-xl dark:prose-invert dark:prose-headings:text-slate-300 prose-headings:font-heading prose-headings:leading-tighter prose-headings:tracking-tighter prose-headings:font-bold prose-img:rounded-md prose-img:shadow-lg mt-8 prose-a:text-black/75 dark:prose-a:text-white/90 prose-a:underline prose-a:underline-offset-4 prose-a:decoration-primary-500 hover:prose-a:decoration-primary-600 prose-a:decoration-2 hover:prose-a:decoration-4 hover:prose-a:text-black dark:hover:prose-a:text-white break-words tracking-normal prose-h4:tracking-normal prose-h5:tracking-normal prose-h6:tracking-normal prose-code:before:hidden prose-code:after:hidden markdown-body">
                 {!! str($post->body)->markdown() !!}
             </section>
+        </div>
 
-            <x-cms-social-share />
-            <div>
+        <div class="flex flex-col sm:flex-row sm:justify-center gap-2">
+            <div class="flex flex-col justify-center sm:justify-end items-center sm:items-end">
                 @livewire(\Modules\Cms\Livewire\LikePost::class, ['post' => $post])
             </div>
+
+            <x-cms-social-share />
         </div>
+
+        @livewire(\Modules\Cms\Livewire\CommentPost::class, ['post' => $post])
     </div>
 @endsection
