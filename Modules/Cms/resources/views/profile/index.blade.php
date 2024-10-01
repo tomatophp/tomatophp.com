3fd1a883ce460677736d04a66162ca6b48b2949f
Fady Mondy <info@3x1.io>
2024-10-01 18:13:07 +0300

    add public profile

--- Modules/Cms/resources/views/profile/index.blade.php
+++ Modules/Cms/resources/views/profile/index.blade.php
@@ -1,8 +1,8 @@
 @extends('cms::layouts.app')
 
 @section('title', (app()->getLocale() === 'en' ? str(setting('site_name'))->explode('|')[0]??setting('site_name') : str(setting('site_name'))->explode('|')[1]??setting('site_name')) . ' | '.$account->name)
-@section('description', $account->short_description)
-@section('keywords', $account->keywords)
+@section('description', $account->meta('bio'))
+@section('keywords', $account->meta('bio'))
 @if($account->getFirstMediaUrl('avatar'))
     @section('image', $account->getFirstMediaUrl('avatar'))
 @endif
@@ -122,6 +122,8 @@
 
                 </div>
             </div>
+
+            <x-cms-social-share />
         </div>
     </div>
 @endsection
