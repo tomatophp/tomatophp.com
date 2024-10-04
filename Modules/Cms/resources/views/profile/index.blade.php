083a62c628e13f69b8174089b40b30c8f6372db3
Fady Mondy <info@3x1.io>
2024-10-04 16:04:49 +0300

    link comments to profile

--- Modules/Cms/resources/views/profile/index.blade.php
+++ Modules/Cms/resources/views/profile/index.blade.php
@@ -1,6 +1,6 @@
 @extends('cms::layouts.app')
 
-@section('title', (app()->getLocale() === 'en' ? str(setting('site_name'))->explode('|')[0]??setting('site_name') : str(setting('site_name'))->explode('|')[1]??setting('site_name')) . ' | '.$account->name)
+@section('title', (app()->getLocale() === 'en' ? str(setting('site_name'))->explode('|')[0]??setting('site_name') : str(setting('site_name'))->explode('|')[1]??setting('site_name')) . ' | '. $account->name)
 @section('description', $account->meta('bio'))
 @section('keywords', $account->meta('bio'))
 @if($account->getFirstMediaUrl('avatar'))
